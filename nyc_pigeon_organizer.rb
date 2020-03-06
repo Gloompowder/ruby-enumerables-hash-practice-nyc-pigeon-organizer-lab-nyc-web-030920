21f97c22bb58f00f61bded98d106b614dcc924c7
William Lin <willlin1996@gmail.com>
2020-03-06 00:27:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -13,7 +13,7 @@ def nyc_pigeon_organizer(data)
               :lives => []
             }
        end
-       binding.pry
+       pigeon_list[each_name][key] << new_key
         end
      end
   end
